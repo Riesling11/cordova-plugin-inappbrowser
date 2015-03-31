fefd85f68c8a602d37246c7b1e642537eb5bf562
Riesling11 <philipp.broemser@gmail.com>
2015-03-31 09:34:33 +0200

    typo fix

--- src/ios/CDVInAppBrowser.m
+++ src/ios/CDVInAppBrowser.m
@@ -959,7 +959,7 @@ return;
     return YES;
 }
 
-(void)dismissViewControllerAnimated:(BOOL)flag completion:(void (^)(void))completion
+- (void)dismissViewControllerAnimated:(BOOL)flag completion:(void (^)(void))completion
 {
     if ( self.presentedViewController) {
     	[super dismissViewControllerAnimated:flag completion:completion];
